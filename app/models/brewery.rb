edb70fd13089c915cdd83d8902d7a962f8a91d02
Simo <simo.kuparinen@helsinki.fi>
2017-02-26 19:00:16 +0200

    viikon 6 tehtävät

---
+++ app/models/brewery.rb
@@ -0,0 +1,12 @@
+class Brewery < ActiveRecord::Base
+  include RatingAverage
+
+  validates :name, presence: true
+  validates :year, numericality: { less_than_or_equal_to: ->(_) { Time.now.year} }
+
+  scope :active, -> { where active:true }
+  scope :retired, -> { where active:[nil,false] }
+
+  has_many :beers, :dependent => :destroy
+  has_many :ratings, :through => :beers
+end
